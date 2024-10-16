ce16344df3fa54bf9c309bcc79155cf8df7b7491
Dash-crypt <dasharod2@gmail.com>
2024-10-16 18:26:06 +0300

    Add files via upload

---
+++ python_16.py
@@ -0,0 +1,34 @@
+'''a = int(input())
+if a > 14:
+    print("Добро пожаловать!")
+elif a == 14:
+    print("Вам 14 лет! Поздравляю!")
+elif a < 14:
+    print("Вам ещё рано!")'''
+
+
+'''a = input()
+if a == 'secret':
+    print('Доступ разрешен')
+else:
+    print('Неверный пароль!')'''
+    
+
+'''a = input()
+if a.isdigit():
+    print('Это цифра!')
+elif a.isalpha():
+    print('Это буква!')
+else:
+    print('Это не цифра и не буква!')'''
+
+
+'''a = int(input())
+if a > 1000:
+    print(a - (a * 0.1))
+elif a > 500:
+    print(a - (a * 0.05))
+else:
+    print(a)'''
+
+
